638af3adb33d6b53c586543a83107243a0401c8c
Coding for the Arts <88927553+cas-c4ta@users.noreply.github.com>
2021-09-14 21:44:00 +0200

    add challenges, fix typos

--- 1_basics/8_variablen2.py
+++ 1_basics/8_variablen2.py
@@ -1,21 +1,54 @@
 """
 Variablen schreiben
 """
-rect(10, 10, 200, 200)
 
-rect(100 + 100, 2 * 100, 400/2, 400 * 0.5)
+newPage(1000, 1000)
 
-posX = 200 + 200
-posY = 5 * 100
-side = 1000 - 700
+rect1x = 200 + 200
+rect1y = 5 * 100
+rect1width = 1000 - 500
+rect1height = 1000 - 600
 
-rect(posX, posY, side, side)
+rect2x = 1000 % 300 # Rest der Division: 100
+rect2y = 5 * 24
+rect2width = width() // 2
+rect2height = height() / 3
+
+rect(rect1x, rect1y, rect1width, rect1height)
+rect(rect2x, rect2y, rect2width, rect2height)
 
 """
-Variablen:
+Die Beispiele hier sind zwar nicht besonders sinvoll.
+Aber du siehst, dass es möglich ist, ganze Rechnungen
+statt einzelner Werte zu schreiben. Beim Ausführen des
+Scripts werden die Rechungen durch ihr Resultat ersetzt.
+
+- Kannst du zwei Rechtecke so positionieren, dass das eine immer
+  das untere linke Viertel der Fläche ausfüllt und das andere
+  jeweils das obere rechte Viertel?
+- Kannst du es so einrichten, dass es auch funktioniert, wenn es
+  newPage("A4") heisst?
+
+
+Namen von Variablen:
     - beginnen mit kleinbuchstaben
     - keine Zahl als erstes Zeichen
     - erlaubt: a-z, A-Z, 0-9, _ 
     - Camel case: fooBarBaz 🐫
     - Snake case: foo_bar_baz 🐍
+
+Abgesehen davon, dass keine Sonderzeichen erlaubt sind und
+dass sie nicht mit einer Ziffer beginnen dürfen, kannst du
+Variablen so benennen wie du willst.
+
+Du musst allenfalls beachten, dass du kein «reserviertes» Wort
+verwendest (siehe Cheatsheet).
+
+Meist ist es gut, den Namen einer Variablen so zu wählen,
+dass du ihren Zweck daran ablesen kannst.
+
+Wenn dir gerade nichts einfällt, sind Nonsens-Namen wie
+"foo" und "bar" zuverlässige Helfer. Aber vergiss nicht,
+sie später mit Suchen/Ersetzen mit etwas sinvollerem zu
+ersetzen.
 """
